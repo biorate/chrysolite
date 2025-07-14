cd9335a45ce0d09d05b7802ad8d13a945b267f34
Leonid Levkin <llevkin@yandex.ru>
2025-07-14 18:25:36 +0300

    feat: embedding index CRUD

--- apps/chrysolite-client/src/view/components/hello/hello.tsx
+++ apps/chrysolite-client/src/view/components/hello/hello.tsx
@@ -1,8 +1,8 @@
 import { styled } from 'styled-components';
 
 export const HelloWorld = styled.div<{ size: number }>`
+  width: 230px;
   margin-top: -400px;
   margin-left: -100px;
-  width: 230px;
   font-size: ${({ size }) => size}px;
 `;
